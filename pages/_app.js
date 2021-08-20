90de811d60ff2cea56ce0bc196e6e3d16809b9d5
alan <alanarostegui@hotmail.com>
2021-08-19 23:05:32 -0500

    feat(_app): add container

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
-import 'minireset.css'
-import '@alanmaranto/components/styles/main.css';
-import '@alanmaranto/components/styles/tokens.css';
+import "minireset.css";
+import "@alanmaranto/components/styles/main.css";
+import "@alanmaranto/components/styles/tokens.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // Move to a Container
+    <div
+      style={{
+        width: 375,
+        padding: "40px 30px",
+        margin: "0 auto",
+        border: "1px solid rebeccapurple",
+      }}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
