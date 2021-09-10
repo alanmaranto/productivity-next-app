aab5cc4e3252d2ee27c11825142cfc2c5e2d81e8
alan <alanarostegui@hotmail.com>
2021-09-09 21:53:38 -0500

    feat: add planning component

--- pages/planning.js
+++ pages/planning.js
@@ -1,10 +1,45 @@
-import { FullHeightContent, Button } from "@alanmaranto/components";
+import { useState } from "react";
+import {
+  FullHeightContent,
+  Button,
+  Avatar,
+  Heading,
+  Spacer,
+} from "@alanmaranto/components";
 
 const Start = () => {
+  const [tasks, setTasks] = useState([]);
   return (
     <FullHeightContent
       content={
-        <div style={{ height: 200, background: "tomato", width: "100%" }}></div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+          }}
+        >
+          <div style={{ display: "flex" }}>
+            <Avatar src="https://placeimg.com/200/200/people" />
+            <Spacer.Vertical size="xs" />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "start",
+              }}
+            >
+              <Heading size="lg">Hola, Alan</Heading>
+              <Heading size="md" color="primary">
+                ¿Cómo quieres empezar?
+              </Heading>
+            </div>
+          </div>
+          <Heading size="lg">
+            Ahora dime, ¿Cuál es la primera tarea en la que trabajarás hoy
+          </Heading>
+          <button></button>
+        </div>
       }
       footer={
         <div>
