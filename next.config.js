30f3c6e30029b98bc19075631199064893140545
alan <alanarostegui@hotmail.com>
2021-10-20 23:05:34 -0500

    refactor: transpile link library components

--- next.config.js
+++ next.config.js
@@ -1,20 +1,3 @@
-const path = require("path");
-const transpileModules = require("next-transpile-modules");
+const withComponentsLibrary = require("./config/withComponentsLibrary");
 
-const reactPath = path.resolve(__dirname, "node_modules/react");
-const withTranspileModules = transpileModules(["@alanmaranto/components"]);
-
-// https://github.com/martpie/next-transpile-modules#i-have-trouble-with-duplicated-dependencies-or-the-invalid-hook-call-error-in-react
-const resolveUniqueReactForHooks = {
-  webpack: (config, options) => {
-    if (options.isServer) {
-      config.externals = ["react", ...config.externals];
-    }
-
-    config.resolve.alias["react"] = reactPath;
-
-    return config;
-  },
-};
-
-module.exports = withTranspileModules(resolveUniqueReactForHooks);
+module.exports = withComponentsLibrary();
