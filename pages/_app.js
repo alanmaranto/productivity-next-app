10eed641098f44a86e7a642ce7717ae00f958bf4
alan <alanarostegui@hotmail.com>
2021-10-24 19:10:40 -0500

    feat: add link and header

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,46 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { Container } from "@alanmaranto/components";
+import { Container, Button } from "@alanmaranto/components";
+import Head from "next/head";
+import Link from "next/link";
 
 import "minireset.css";
 import "@alanmaranto/components/styles/main.css";
 import "@alanmaranto/components/styles/tokens.css";
+import '../styles/globals.css';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+        }}
+      >
+        {["/", "/home", "/planning"].map((link) => (
+          <span key={link} style={{ marginRight: 10 }}>
+            <Link passHref href={link}>
+              <Button type="tertiary">
+                {link.replace("/", "") || "/index"}
+              </Button>
+            </Link>
+          </span>
+        ))}
+      </div>
       <Container>
         <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
       </Container>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
